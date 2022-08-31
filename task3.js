ef344d35aa7de3265857d3cc3b75241b984384aa
Aimee Laws <aimee.laws@luxinar.com>
2022-08-31 15:27:51 +0100

    submitted task 3

---
+++ task3.js
@@ -0,0 +1,11 @@
+// create a function with 2 arguments firstName and lastName that returns an object with the keys firstName & lastName
+
+const user = (firstName, lastName) => {
+  const fullName = {
+    firstName: firstName,
+    lastName: lastName
+  };
+  return fullName;
+};
+
+console.log(user('Jane', 'Doe'));
