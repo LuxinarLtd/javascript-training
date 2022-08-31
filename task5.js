47bcf43abdc76ac624f0348d24f1df1a2182478d
Aimee Laws <aimee.laws@luxinar.com>
2022-08-31 15:52:27 +0100

    submitted task 5

---
+++ task5.js
@@ -0,0 +1,8 @@
+// add the name 'Aimee' to the array and remove the name 'Lewis' from the array
+
+const itTeam = ['Chris', 'Lewis', 'Ian'];
+
+itTeam.push('Aimee');
+itTeam.splice(1, 1);
+
+console.log(itTeam);
