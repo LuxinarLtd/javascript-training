25e83a5ca3f59f3e596286d1ce134509b4448bf2
Aimee Laws <aimee.laws@luxinar.com>
2022-08-31 16:37:57 +0100

    submitted task 8

---
+++ task8.js
@@ -0,0 +1,9 @@
+// console each name saying i.e. 'Lewis well done!'
+
+const itTeam = ['Chris', 'Lewis', 'Ian', 'Aimee'];
+let count = 0;
+
+for (let i = 0; i < itTeam.length; i++) {
+  console.log(`${itTeam[count]} well done!`)
+  count++
+};
