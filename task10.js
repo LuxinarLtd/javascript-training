bc8a997f01c90e96b62de13859701e8a71ce50a5
Aimee Laws <aimee.laws@luxinar.com>
2022-08-31 16:59:08 +0100

    submitted task 10

---
+++ task10.js
@@ -0,0 +1,30 @@
+// filter the array with people who have a higher score than 3 and console their names saying they have passed i.e. 'Lewis has Passed'
+
+const users = [
+  {
+    name: 'Lewis',
+    gender: 'male',
+    score: 10
+  },
+  {
+    name: 'Chris',
+    gender: 'male',
+    score: 7
+  },
+  {
+    name: 'Aimee',
+    gender: 'female',
+    score: 4
+  },
+  {
+    name: 'Ian',
+    gender: 'female',
+    score: 2
+  }
+]
+
+const result = users.filter(user => {
+  if (user.score > 3) {
+    console.log(`${user.name} has Passed`);
+  }
+});
