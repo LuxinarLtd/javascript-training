ef487fe32463598c485f7b76b73937b0550bf66f
Aimee Laws <aimee.laws@luxinar.com>
2022-09-01 15:53:58 +0100

    resubmitted task 18

--- task18.js
+++ task18.js
@@ -1,5 +1,7 @@
 // return the current date, ISO week number and how many days are left in the current year
 
+const moment = require('moment');
+
 let today = new Date();
 let day = String(today.getDate()).padStart(2, '0');
 let month = String(today.getMonth() + 1).padStart(2, '0');
@@ -8,20 +10,12 @@ const endOfYear = new Date("12/31/2022");
 
 todayDate = day + '/' + month + '/' + year;
 
-function daysInMonth(month, year) {
-  return new Date(year, month, 0).getDate();
-}
-
-function daysInYear(year) {
-    return ((year % 4 === 0 && year % 100 > 0) || year %400 == 0) ? 366 : 365;
-}
-
 const oneDay = 1000 * 60 * 60 * 24;
 const diffInTime = endOfYear.getTime() - today.getTime();
 const diffInDays = Math.round(diffInTime / oneDay);
 
 const dayOfWeek = today.getDay();
-const currentWeek = Math.floor(month * 4.34);
+const currentWeek = moment().format('W');
 
 console.log(todayDate);
 console.log(diffInDays);
