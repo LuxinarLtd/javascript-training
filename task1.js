af1f517b93b99d03c6f4077f144d892a96953514
Aimee Laws <aimee.laws@luxinar.com>
2022-09-01 16:26:06 +0100

    task 1 improvements

--- task1.js
+++ task1.js
@@ -1,7 +1,13 @@
 // Create a function that prints out a full name by entering the arguments first name and last name
 
-const fullName = ((firstName, lastName) => {
+const fullName = (firstName, lastName) => {
   return firstName + lastName;
-});
+};
 
 console.log(fullName('Emily ', 'Doe'));
+
+// Imporoved
+
+const name = (firstName, lastName) => `${firstName} ${lastName}`;
+
+console.log(name('Jane', 'Doe'));
