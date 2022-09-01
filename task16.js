e14be7ef2e5603d518e49c05d414b31bedc984a6
Aimee Laws <aimee.laws@luxinar.com>
2022-09-01 12:23:03 +0100

    submitted task 16

---
+++ task16.js
@@ -0,0 +1,9 @@
+// print the numbers that are divisible by 5 from the list below
+
+const numbers = [10, 20, 33, 46, 55, 72];
+
+const factor5 = numbers(number => {
+  return number / 5;
+});
+
+numbers.filter(factor5);
