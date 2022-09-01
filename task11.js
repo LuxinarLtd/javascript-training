c938ccbe2e81c5b9768f4de2507859ac22931411
Aimee Laws <aimee.laws@luxinar.com>
2022-09-01 09:04:12 +0100

    submitted task 11

---
+++ task11.js
@@ -0,0 +1,17 @@
+// return the object with the firstName "Jacob"
+
+const people = [
+  { firstName: "Susan", age: 14 },
+  { firstName: "Daniel", age: 16 },
+  { firstName: "Bruno", age: 56 },
+  { firstName: "Jacob", age: 15 },
+  { firstName: "Sam", age: 64 },
+  { firstName: "Dave", age: 56 },
+  { firstName: "Neils", age: 65 }
+];
+
+const person = people.filter(arr => {
+  return arr.firstName === "Jacob"
+});
+
+console.log(person);
