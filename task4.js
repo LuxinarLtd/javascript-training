7dab1160463a7290f5775cebcd2e8db09467052b
Aimee Laws <aimee.laws@luxinar.com>
2022-09-02 15:26:18 +0100

    task 4 improvements

--- task4.js
+++ task4.js
@@ -13,4 +13,26 @@ const number = (inputNo => {
   return inputNo
 });
 
-console.log(number(5));
+// console.log(number(5));
+
+// Improved
+
+const number2 = (inputNo => {
+  if (inputNo > 10) {
+    console.log("The number is more than 10")
+  } else if (inputNo < 10) {
+    console.log("The number is less than 10")
+  } else if (inputNo === 10) {
+    console.log("The number is equal to 10")
+  }
+});
+
+number2(5);
+
+const number3 = (inputNo => {
+  if (inputNo > 10) return "The number is more than 10";
+  if (inputNo < 10) return "The number is less than 10";
+  if (inputNo === 10) return "The number is equal to 10";
+});
+
+console.log(number3(5));
