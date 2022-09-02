dc8743177b6f0092e62ec8f2ba8204ff0316449b
Aimee Laws <aimee.laws@luxinar.com>
2022-09-02 15:37:08 +0100

    task 7 improvements

--- task7.js
+++ task7.js
@@ -5,3 +5,11 @@ const numbers = [22, 42, 17, 2, 7, 88];
 const higherNumbers = numbers.map(number => number + 2);
 
 console.log(higherNumbers);
+
+// alternative
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i] += 2;
+}
+
+console.log(numbers);
