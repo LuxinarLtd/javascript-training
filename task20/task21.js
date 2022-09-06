cd87e980e302aa141e46aebec9cf9829d96b7007
Aimee Laws <aimee.laws@luxinar.com>
2022-09-06 15:48:04 +0100

    submitted task 21

---
+++ task20/task21.js
@@ -0,0 +1,18 @@
+// create a button that generates a random number every time it is clicked and displays the number.
+
+const btn = document.createElement('button');
+const message = document.createElement('h1');
+const container = document.getElementById('container');
+container.appendChild(btn);
+container.appendChild(message);
+
+btn.style.width = '100px';
+btn.style.height = '50px';
+btn.textContent = 'Generate number';
+
+btn.addEventListener('click', (e) => {
+  const randomRange = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+  const randomNumber = randomRange(1, 50);
+  message.textContent = `${randomNumber}`;
+});
+
