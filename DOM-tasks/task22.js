dbc0babd236415a6a479808e682cc0c605b7e503
Aimee Laws <aimee.laws@luxinar.com>
2022-09-06 16:10:22 +0100

    submitted task 22

---
+++ DOM-tasks/task22.js
@@ -0,0 +1,22 @@
+// create a button that changes the web pages background colour to a random colour using RGB. This will also display the RGB colour code.
+
+const btn = document.createElement('button');
+const message = document.createElement('h1');
+const container = document.getElementById('container');
+container.appendChild(btn);
+container.appendChild(message);
+
+btn.style.width = '100px';
+btn.style.height = '50px';
+btn.textContent = 'Click here';
+
+btn.addEventListener('click', (e) => {
+  const randomRange = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+  const randomR = randomRange(0, 255);
+  const randomG = randomRange(0, 255);
+  const randomB = randomRange(0, 255);
+
+  document.body.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+  message.textContent = `rgb(${randomR}, ${randomG}, ${randomB})`;
+});
+
